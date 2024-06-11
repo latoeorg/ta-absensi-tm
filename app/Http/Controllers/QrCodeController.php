ba1268b19aa706f37685d073811add7f8b80b7ea
lazlanrafar <lazlanrafar@gmail.com>
2024-06-11 18:42:17 +0800

    new things coming

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Log;
 
 class QrCodeController extends Controller
 {
-
     public function generate()
     {
         // Check if the user is a super admin
@@ -28,7 +27,7 @@ class QrCodeController extends Controller
         if (!$qrCode) {
             // Generate the QR code
             $baseUrl = 'http://' . request()->getHttpHost(); // Get the current domain or IP address
-            $qrCodeData = $baseUrl . route('login-attendance'); // Use the appropriate route name here
+            $qrCodeData = 'http://192.168.0.46:8000/scan-qr'; // Use the appropriate route name here
 
             $result = Builder::create()
                 ->writer(new PngWriter())
@@ -42,42 +41,43 @@ class QrCodeController extends Controller
 
             $qrCode = new QrCode([
                 'code' => base64_encode($result->getString()),
-                'date' => Carbon::now()
+                'date' => Carbon::now(),
             ]);
             $qrCode->save();
         }
 
         return view('pages.qr.generate', [
             'qrCode' => $qrCode,
-            'isSuperAdmin' => $isSuperAdmin
+            'isSuperAdmin' => $isSuperAdmin,
         ]);
     }
 
     public function scan()
-{
-    $date = Carbon::now(new DateTimeZone('Asia/Bangkok'))->toDateString();
-    $user = Auth::user();
+    {
+        $date = Carbon::now(new DateTimeZone('Asia/Bangkok'))->toDateString();
+        $user = Auth::user();
+        if (!$user) {
+            return redirect()->route('login-attendance');
+        }
+        // Check if the user already has clocked in today
+        $attendance = $user->attendances()->where('date', $date)->first();
 
-    // Check if the user already has clocked in today
-    $attendance = $user->attendances()->where('date', $date)->first();
+        if ($attendance) {
+            // Clock out
+            $attendance->clock_out = Carbon::now(new DateTimeZone('Asia/Bangkok'));
+            $attendance->save();
+            $message = 'Clocked out successfully';
+            Log::info('User ' . $user->name . ' clocked out at ' . $attendance->clock_out->toDateTimeString());
+        } else {
+            // Clock in
+            $attendance = $user->attendances()->create([
+                'date' => $date,
+                'clock_in' => Carbon::now(new DateTimeZone('Asia/Bangkok')),
+            ]);
+            $message = 'Clocked in successfully';
+            Log::info('User ' . $user->name . ' clocked in at ' . $attendance->clock_in->toDateTimeString());
+        }
 
-    if ($attendance) {
-        // Clock out
-        $attendance->clock_out = Carbon::now(new DateTimeZone('Asia/Bangkok'));
-        $attendance->save();
-        $message = 'Clocked out successfully';
-        Log::info("User " . $user->name . " clocked out at " . $attendance->clock_out->toDateTimeString());
-    } else {
-        // Clock in
-        $attendance = $user->attendances()->create([
-            'date' => $date,
-            'clock_in' => Carbon::now(new DateTimeZone('Asia/Bangkok'))
-        ]);
-        $message = 'Clocked in successfully';
-        Log::info("User " . $user->name . " clocked in at " . $attendance->clock_in->toDateTimeString());
+        return redirect()->route('attendance.index')->with('status', $message);
     }
-
-    return redirect()->route('attendance.index')->with('status', $message);
-}
-
 }
--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,26 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\QrCodeController;
 // routes/web.php
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/attendance/all', [AttendanceController::class, 'allAttendance'])->name('attendance.all');
 });
 
-Route::get('/generate-qr', [QrCodeController::class, 'generate'])->name('qr.generate')->middleware('auth');
-Route::get('/scan-qr', [QrCodeController::class, 'scan'])->name('attendance.scan')->middleware('auth');
+Route::get('/generate-qr', [QrCodeController::class, 'generate'])
+    ->name('qr.generate')
+    ->middleware('auth');
+Route::get('/scan-qr', [QrCodeController::class, 'scan'])
+    ->name('attendance.scan')
+    ->middleware('redirect_if_not_authenticated');
 
 // AUTH
-Route::get('/login', [AuthController::class, 'index'])->name('login')->middleware('guest');
+Route::get('/login', [AuthController::class, 'index'])
+    ->name('login')
+    ->middleware('guest');
 Route::post('/login', [AuthController::class, 'authenticate'])->middleware('guest');
-Route::get('/login-attendance', [AuthController::class, 'index'])->name('login-attendance')->middleware('guest');
+Route::get('/login-attendance', [AuthController::class, 'index'])
+    ->name('login-attendance')
+    ->middleware('guest');
+
 Route::post('/login-attendance', [AuthController::class, 'authenticateAttendance'])->middleware('guest');
 Route::post('/logout', [AuthController::class, 'logout']);
 
@@ -30,7 +38,9 @@ Route::resource('/', AttendanceController::class)->middleware('auth');
 Route::resource('/user', UserController::class)->middleware('auth');
 
 // PROFILE
-Route::get('/update-profile', [UserController::class, 'editProfile'])->name('update-profile')->middleware('auth');
+Route::get('/update-profile', [UserController::class, 'editProfile'])
+    ->name('update-profile')
+    ->middleware('auth');
 
 // ATTENDANCE
 Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
